25fee77b4d980fe56294dd725468348b538ab96e
Leonardo Syma <leosyma@gmail.com>
2024-10-09 19:31:20 -0300

    Atualização do botão

--- 1_home.py
+++ 1_home.py
@@ -19,9 +19,9 @@ st.markdown('# FIFA23 OFFICIAL DATASET! ⚽')
 st.sidebar.markdown('Desenvolvido por Leonardo Syma')
 
 # Adiciona um botão para acessar o site dos dados
-btn = st.button('Acesse os dados no Kaggle')
-if btn:
-    webbrowser.open_new_tab('https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data')
+btn = st.link_button('Acesse os dados no Kaggle',
+                     'https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data')
+
 
 # Descrição dos dados
 st.markdown(
